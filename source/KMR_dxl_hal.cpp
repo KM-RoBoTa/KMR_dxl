85124ca4588df644907f09d0a36b9b8d0ad7ab03
klichardova <katarina.lichardova@gmail.com>
2023-09-15 16:42:29 +0200

    Update authors

--- source/KMR_dxl_hal.cpp
+++ source/KMR_dxl_hal.cpp
@@ -17,9 +17,10 @@
  ******************************************************************************
  * @copyright
  * Copyright 2021-2023 Laura Paez Coy and Kamilo Melo                    \n
- * This code is under MIT licence: https://opensource.org/licenses/MIT  !!!!!
- * @authors  Laura.Paez@KM-RoBota.com, 04-2023  !!!
- * @authors  Kamilo.Melo@KM-RoBota.com, 05-2023
+ * This code is under MIT licence: https://opensource.org/licenses/MIT  
+ * @authors  Laura.Paez@KM-RoBota.com, 08/2023
+ * @authors  Kamilo.Melo@KM-RoBota.com, 08/2023
+ * @authors katarina.lichardova@km-robota.com, 08/2023
  ******************************************************************************
  */
 
